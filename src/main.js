966ccb866cfb9ed9636d188584e30f3ee3243b5a
getGrovy <125417752+getGrovy@users.noreply.github.com>
2023-06-23 10:46:44 +0900

    first

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import './scss/style.scss';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyDfg0hWi_KL2I0ufeNZIynchEO6o8xQPU8',
+    authDomain: 'getgrovy-port.firebaseapp.com',
+    projectId: 'getgrovy-port',
+    storageBucket: 'getgrovy-port.appspot.com',
+    messagingSenderId: '95420219653',
+    appId: '1:95420219653:web:66833f5cf6fe0f0a48b76c',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
